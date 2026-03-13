32b180afd26778c0aca753b2fcc5447d459bd433
jeshjangid <jeshjangid09@gmail.com>
2026-03-13 21:49:58 +0530

    url updated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // const API = "http://localhost:5000/api"
-const API = "https://stripe-test-puce.vercel.app/api"
+const API = "https://stripe-test-g0wy.onrender.com/api"
 
 export const createCheckout = ()=>{
 
